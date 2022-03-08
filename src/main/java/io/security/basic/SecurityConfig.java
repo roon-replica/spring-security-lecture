c96e9dc9252524467cf1eab800f44425648c9f1c
leemr <think-roon@naver.com>
2022-03-08 14:27:17 +0900

    [chore] 불필요 코드 제거

--- src/main/java/io/security/basic/SecurityConfig.java
+++ src/main/java/io/security/basic/SecurityConfig.java
@@ -4,23 +4,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
 import java.util.ArrayList;
 
 @EnableWebSecurity
@@ -58,7 +45,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutSuccessHandler((request, response, authentication) -> response.sendRedirect("/login"))
                 .deleteCookies("remember-me");
 
-        //test용이라 익명 클래스로 작성
         http.rememberMe()
                 .rememberMeParameter("remember")    // default : remember-me
                 .tokenValiditySeconds(30)   // set 30 seconds for test

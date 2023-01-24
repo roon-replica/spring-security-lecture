08ee4853c9957bd533b2181b59c81197998b7d15
roon <think-roon@naver.com>
2023-01-24 15:03:44 +0900

    [실전프로젝트-Form 인증 프로세스 구현] web ignore 설정

--- src/main/java/io/security/project/security/SecurityConfig.java
+++ src/main/java/io/security/project/security/SecurityConfig.java
@@ -1,9 +1,11 @@
 package io.security.project.security;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -33,6 +35,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
 		auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER", "USER");
 		auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN", "MANAGER", "USER");
+	}
+
+	@Override
+	public void configure(WebSecurity web) {
+		web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
 	}
 
 	@Bean
